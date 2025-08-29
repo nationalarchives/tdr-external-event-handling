66fd3d6ac682ef8e7d0d79d43df5044358438922
Tom Hallett <tom.hallett@nationalarchives.gov.uk>
2025-08-29 16:25:09 +0100

    Updating DR2 Decoder

--- src/main/scala/uk/gov/nationalarchives/externalevent/LambdaRunner.scala
+++ src/main/scala/uk/gov/nationalarchives/externalevent/LambdaRunner.scala
@@ -16,16 +16,18 @@ object LambdaRunner extends App {
 
   val DR2SQSMessage1 = s"""
   {
-    "properties": {
-      "executionId": "TESTDOC_TDR-2021-CMTP_0",
-      "messageId": "c4a1c1af-dd03-437d-b7e7-ba029d1afc4f",
-      "parentMessageId": null,
-      "timestamp": "2025-01-31T16:07:49.129278081Z",
-      "messageType": "preserve.digital.asset.ingest.complete"
-    },
-    "parameters": {
-      "assetId": "${TestAssetID1}",
-      "status": "Asset has been written to custodial copy disk."
+    "body": {
+      "properties": {
+        "executionId": "TESTDOC_TDR-2021-CMTP_0",
+        "messageId": "c4a1c1af-dd03-437d-b7e7-ba029d1afc4f",
+        "parentMessageId": null,
+        "timestamp": "2025-01-31T16:07:49.129278081Z",
+        "messageType": "preserve.digital.asset.ingest.complete"
+      },
+      "parameters": {
+        "assetId": "${TestAssetID1}",
+        "status": "Asset has been written to custodial copy disk."
+      }
     },
     "timestamp": "1738339669217",
     "topicArn": "arn:aws:sns:eu-west-2:XXXXXXXXXXXX:intg-dr2-notifications"
@@ -34,16 +36,18 @@ object LambdaRunner extends App {
 
   val DR2SQSMessage2 = s"""
   {
-    "properties": {
-      "executionId": "TESTDOC_TDR-2022-CMTP_2",
-      "messageId": "cb8c1adc-b7c1-43db-a4cb-cd034a25186e ",
-      "parentMessageId": null,
-      "timestamp": "2025-03-01T16:07:49.129278081Z",
-      "messageType": "preserve.digital.asset.ingest.success"
-    },
-    "parameters": {
-      "assetId": "${TestAssetID2}",
-      "status": "Asset has been written to custodial copy disk."
+    "body": {
+      "properties": {
+        "executionId": "TESTDOC_TDR-2022-CMTP_2",
+        "messageId": "cb8c1adc-b7c1-43db-a4cb-cd034a25186e ",
+        "parentMessageId": null,
+        "timestamp": "2025-03-01T16:07:49.129278081Z",
+        "messageType": "preserve.digital.asset.ingest.success"
+      },
+      "parameters": {
+        "assetId": "${TestAssetID2}",
+        "status": "Asset has been written to custodial copy disk."
+      }
     },
     "timestamp": "1738339669217",
     "topicArn": "arn:aws:sqs:eu-west-2:XXXXXXXXXXXX:intg-dr2-notifications"
